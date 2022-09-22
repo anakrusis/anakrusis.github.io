82be5a8474051993af93baf3c83d4e7a8b44d98f
anakrusis <howtofamitracker@gmail.com>
2022-09-22 01:34:42 -0400

    update emojii

---
+++ games/glyphs/glyphs.js
@@ -0,0 +1,371 @@
+// The main name of a glyph is the same as its filename in the folder
+GLYPHS = {
+	// these glyphs are primarily (but not always) used phonetically
+	"eye": {
+		"aliases": ["'"],
+		"use": "placeholder for words with initial vowel"
+	},
+	"bee": {
+		"aliases": ["b"],
+		"use": "uniliteral /b/, be"
+	},
+	"bell": {
+		"aliases": ["bl"],
+		"use": "biliteral /bl/, -ble"
+	},
+	"hatching_chick": {
+		"aliases": ["ch"],
+		"use": "uniliteral /tʃ/"
+	},
+	"door": {
+		"aliases": ["d"],
+		"use": "uniliteral /d/"
+	},
+	"fire": {
+		"aliases": ["f"],
+		"use": "uniliteral /f/"
+	},
+	"gear": {
+		"aliases": ["g"],
+		"use": "uniliteral /g/"
+	},
+	"rabbit": {
+		"aliases": ["h"],
+		"use": "uniliteral /h/ ('hare')"
+	},
+	"jar": {
+		"aliases": ["j"],
+		"use": "uniliteral /dʒ/, /ʒ/"
+	},
+	"key": {
+		"aliases": ["k"],
+		"use": "uniliteral /k/"
+	},
+	"call_me": {
+		"aliases": ["kl"],
+		"use": "biliteral /kl/"
+	},
+	"red_car": {
+		"aliases": ["kr"],
+		"use": "biliteral /kr/"
+	},
+	"leaves": {
+		"aliases": ["l"],
+		"use": "uniliteral /l/"
+	},
+	"house": {
+		"aliases": ["m"],
+		"use": "uniliteral /m/ ('home')"
+	},
+	"mountain": {
+		"aliases": ["mount", "mnt"],
+		"use": "triliteral /mnt/"
+	},
+	"leg": {
+		"aliases": ["n"],
+		"use": "uniliteral /n/ ('knee')"
+	},
+	"raised_hand": {
+		"aliases": ["nd"],
+		"use": "biliteral /nd/, and"
+	},
+	"horse": {
+		"aliases": ["ng"],
+		"use": "uniliteral /ŋ/ ('nag')"
+	},
+	"sauropod": {
+		"aliases": ["ns"],
+		"use": "biliteral /ns/ ('Nessie')"
+	},
+	"knot": {
+		"aliases": ["nt"],
+		"use": "biliteral /nt/"
+	},
+	"feet": {
+		"aliases": ["p"],
+		"use": "uniliteral /p/ ('paw')"
+	},
+	"person_swimming": {
+		"aliases": ["pl"],
+		"use": "biliteral /pl/ ('pool')"
+	},
+	"pear": {
+		"aliases": ["pr"],
+		"use": "biliteral /pr/"
+	},
+	"ear": {
+		"aliases": ["r"],
+		"use": "uniliteral /r/"
+	},
+	"ice_cube": {
+		"aliases": ["s"],
+		"use": "uniliteral /s/"
+	},
+	"spoon": {
+		"aliases": ["sp", "sb"],
+		"use": "biliteral /sp/"
+	},
+	"star": {
+		"aliases": ["st", "sd"],
+		"use": "biliteral /st/"
+	},
+	"socks": {
+		"aliases": ["sk", "sg"],
+		"use": "biliteral /sk/"
+	},
+	"athletic_shoe": {
+		"aliases": ["sh"],
+		"use": "uniliteral /ʃ/"
+	},
+	"sparkles": {
+		"aliases": ["shn"],
+		"use": "biliteral /ʃn/ ('shine')"
+	},
+	"sailboat": {
+		"aliases": ["shp"],
+		"use": "biliteral /ʃp/ ('ship')"
+	},
+	"sun_with_face": {
+		"aliases": ["sun", "sn"],
+		"use": "biliteral /sn/"
+	},
+	"tea": {
+		"aliases": ["t"],
+		"use": "uniliteral /t/, to"
+	},
+	"coffee": {
+		"aliases": ["T"],
+		"use": "uniliteral /ɾ/ (tapped T)"
+	},
+	"thread": {
+		"aliases": ["th"],
+		"use": "uniliteral /θ/, /ð/, the"
+	},
+	"tree": {
+		"aliases": ["tr"],
+		"use": "biliteral /tr/"
+	},
+	"ocean": {
+		"aliases": ["v"],
+		"use": "uniliteral /v/, of"
+	},
+	"whale": {
+		"aliases": ["w"],
+		"use": "uniliteral /w/"
+	},
+	"sweet_potato": {
+		"aliases": ["yam","y"],
+		"use": "uniliteral /j/"
+	},
+	"cloud": {
+		"aliases": ["z"],
+		"use": "uniliteral /z/ ('haze')"
+	},
+	
+	// these glyphs are used logographically only
+	
+	"point_up": {
+		"use": "context-specific pronoun, commonly 3p. 'he/she/they/etc.'"
+	},
+	"point_up_2": {
+		"use": "\""
+	},
+	"point_down": {
+		"use": "context-specific pronoun, commonly 3p. 'he/she/they/etc.'"
+	},
+	"point_left": {
+		"use": "context-specific pronoun, commonly 1p. 'I/we' or proximal 'this/here'"
+	},
+	"point_right": {
+		"use": "context-specific pronoun, commonly 2p. 'you' or distal 'those/there'"
+	},
+	"inbox_tray": {
+		"aliases": ["in"],
+		"use": "in, input, get, receive"
+	},
+	"outbox_tray": {
+		"aliases": ["out"],
+		"use": "out, output"
+	},
+	"no_entry_sign": {
+		"aliases": ["not", "no"],
+		"use": "negation"
+	},
+	"chart_with_upwards_trend": {
+		"aliases": ["more"],
+		"use": "more, most, very, etc."
+	},
+	"chart_with_downwards_trend": {
+		"aliases": ["less"],
+		"use": "less, least, hardly, etc."
+	},
+	"thumbsup": {
+		"use": "good"
+	},
+	"thumbsdown": {
+		"use": "bad"
+	},
+	"100": {
+		"use": "a positive intensifier, e.g. 'swag', 'cool'..."
+	},
+	"question": {
+	},
+	"grey_question": {
+	},
+	"clock": {
+		"use": "words relating to time, 'now', 'then'..."
+	},
+	"timer": {
+		"use": "\""
+	},
+	"koko": {
+	},
+	
+	"1234": {
+		"use": "amount, number"
+	},
+	"abcd": {
+	},
+	"symbols": {
+		"use": "characters, glyphs" 
+	},
+	"a": {
+		"use": "\""
+	},
+	"accept": {
+		"use": "can, able, possible"
+	},
+	"arrow_up":{
+	},
+	"arrow_down":{
+	},
+	"arrow_left":{
+	},
+	"arrow_right":{
+	},
+	"arrow_heading_up":{
+	},
+	"arrow_heading_down":{
+	},
+	"left_right_arrow": {
+	},
+	"bird": {
+		"use": "archaic uniliteral /dʒ/"
+	},
+	"book": {
+	},
+	"bulb": {
+	},
+	"bust_in_silhouette": {
+		"use": "people/roles with suffix '-er'"
+	},
+	"camera": {
+	},
+	"checkered_flag": {
+		"use": "end, finish, done"
+	},
+	"cloud_lightning": {
+	},
+	"computer": {
+	},
+	"construction_site": {
+	},
+	"dash": {
+	},
+	"electric_plug": {
+	},
+	"eyes": {
+		"use": "see, look"
+	},
+	"family": {
+		"use": "group"
+	},
+	"game_die": {
+	},
+	"globe_with_meridians": {
+		"aliases": ["internet"],
+		"use": "internet, web"
+	},
+	"hammer": {
+	},
+	"handshake": {
+		"use": "agreement, collaboration"
+	},
+	"hug": {
+	},
+	"loud_sound": {
+	},
+	"map": {
+	},
+	"mobile_phone": {
+	},
+	"muscle": {
+		"use": "strong, hard"
+	},
+	"musical_note": {
+	},
+	"notes": {
+	},
+	"office": {
+		"use": "companies, organizations"
+	},
+	"pinching_hand": {
+		"use": "small, 'micro-'"
+	},
+	"pray": {
+		"use": "hope, wish"
+	},
+	"printer": {
+	},
+	"robot": {
+		"use": "machines with suffix '-er', e.g. 'printer', 'dishwasher', 'assembler'"
+	},
+	"scales": {
+	},
+	"scroll": {
+		"use": "body of text, paragraph, etc."
+	},
+	"seedling": {
+		"use": "begin, start, initial"
+	},
+	"shopping_cart": {
+	},
+	"sound": {
+	},
+	"speaking_head": {
+		"use": "speech, spoken language"
+	},
+	"speech_balloon": {
+	},
+	"test_tube": {
+		"use": "try, test, research, experiment"
+	},
+	"thought_balloon": {
+		"use": "idea, concept"
+	},
+	"tongue": {
+		"use": "language, licking"
+	},
+	"tools": {
+	},
+	"tooth": {
+	},
+	"wave": {
+		"use": "greetings, 'hello'"
+	},
+	"white_heart": {
+		"use": "soul, mind, inner workings"
+	},
+	"wrench": {
+	},
+	"writing_hand": {
+	},
+	
+	"confounded": {
+	},
+	"relaxed": {
+		"use": "calm"
+	},
+	
+	"NO_EMOJI": {}
+}
--- games/glyphs/main.js
+++ games/glyphs/main.js
@@ -1,90 +1,62 @@
-TEXT_COLUMNS = 15;
+TEXT_COLUMNS = 32;
 SPACES_DRAWN = true;
-CHAR_DIM = 72;
+TRANSPARENT = true;
+CHAR_DIM = 48;
 CHAR_PADDING = 4;
 
 function setup(){
 	document.getElementById("btn").onclick = function(){
 		SPACES_DRAWN = document.getElementById("spacecb").checked;
+		TRANSPARENT = document.getElementById("transcb").checked;
 		var text = document.getElementById("textarea").value;
 		transcribeText(text);
 	}
+	
+	textAlign(LEFT, TOP)
+	textSize(CHAR_DIM);
+	fill(255)
 }
 
 function preload(){
-	GLYPHS = {
-		// phonetic glyphs first
-		"'":   loadImage("emoji/eye.png"),
-		"b":   loadImage("emoji/bee.png"),
-		"bl":  loadImage("emoji/bell.png"),
-		"ch":  loadImage("emoji/hatching_chick.png"),
-		"d":   loadImage("emoji/door.png"),
-		"f":   loadImage("emoji/fire.png"),
-		"g":   loadImage("emoji/gear.png"),
-		"h":   loadImage("emoji/rabbit.png"),
-		"j":   loadImage("emoji/jar.png"),
-		"k":   loadImage("emoji/key.png"),
-		"kl":  loadImage("emoji/red_car.png"),
-		"kr":  loadImage("emoji/call_me.png"),
-		"l":   loadImage("emoji/leaves.png"),
-		"m":   loadImage("emoji/house.png"),
-		"n":   loadImage("emoji/leg.png"),
-		"nd":  loadImage("emoji/raised_hand.png"),
-		"ng":  loadImage("emoji/horse.png"),
-		"ns":  loadImage("emoji/sauropod.png"),
-		"nt":  loadImage("emoji/knot.png"),
-		"p":   loadImage("emoji/feet.png"),
-		"pl":  loadImage("emoji/person_swimming.png"),
-		"pr":  loadImage("emoji/pear.png"),
-		"r":   loadImage("emoji/ear.png"),
-		"s":   loadImage("emoji/ice_cube.png"),
-		"sb":  loadImage("emoji/spoon.png"),
-		"sd":  loadImage("emoji/star.png"),
-		"sg":  loadImage("emoji/socks.png"),
-		"sh":  loadImage("emoji/athletic_shoe.png"),
-		"shn": loadImage("emoji/sparkles.png"),
-		"shp": loadImage("emoji/sailboat.png"),
-		"t":   loadImage("emoji/tea.png"),
-		"T":   loadImage("emoji/coffee.png"),
-		"th":  loadImage("emoji/thread.png"),
-		"tr":  loadImage("emoji/tree.png"),
-		"v":   loadImage("emoji/ocean.png"),
-		"w":   loadImage("emoji/whale.png"),
-		"y":   loadImage("emoji/sweet_potato.png"),
-		"z":   loadImage("emoji/cloud.png"),
+	var dict = document.getElementById("dictionary")
+	
+	for (key in GLYPHS) {
+		var names = "[" + key + "], "
+		glyph = GLYPHS[key];
+		// loads image associated with glyph's name
+		glyph.img = loadImage("emoji/" + key + ".png");
 		
-		// logograms next...
-		// (I'll just add them as they are needed)
-		"point_up": 		loadImage("emoji/point_up.png"),
-		"point_down": 		loadImage("emoji/point_down.png"),
-		"point_left": 		loadImage("emoji/point_left.png"),
-		"point_right": 		loadImage("emoji/point_right.png"),
-		"point_up_2":		loadImage("emoji/point_up_2.png"),
-		"wave": 			loadImage("emoji/wave.png"),
-		"inbox_tray":		loadImage("emoji/inbox_tray.png"),
-		"outbox_tray":		loadImage("emoji/outbox_tray.png"),
-		"koko":				loadImage("emoji/koko.png"),
-		"clock":			loadImage("emoji/clock.png"),
-		"left_right_arrow":	loadImage("emoji/left_right_arrow.png"),
-		"question":			loadImage("emoji/question.png"),
-		"grey_question":	loadImage("emoji/grey_question.png"),
+		if (key == "NO_EMOJI"){ continue; }
 		
-		"test_tube":		loadImage("emoji/test_tube.png"),
-		"bulb":				loadImage("emoji/bulb.png"),
-		"book":				loadImage("emoji/book.png"),
-		"writing_hand":		loadImage("emoji/writing_hand.png"),
-		"hammer": 			loadImage("emoji/hammer.png"),
-		"wrench": 			loadImage("emoji/wrench.png"),
-		"tools": 			loadImage("emoji/tools.png"),
+		// maps all aliases to the same pointer as the glyph itself
+		if (glyph.aliases){
+			for (var i = 0; i < glyph.aliases.length; i++){
+				var ca = glyph.aliases[i];
+				GLYPHS[ca] = glyph;
+				
+				var aliasstring = ca;
+				if (aliasstring.length > 1){
+					aliasstring = "[" + aliasstring + "]";
+				}
+				
+				names += aliasstring + ", ";
+			}
+		}
+		// gets rid of the extra comma and space at the end
+		names = names.slice(0, -2)
 		
-		"computer":			loadImage("emoji/computer.png"),
-		"scroll":			loadImage("emoji/scroll.png"),
+		// generates a dictionary entry for every emoji
+		var newrow = dict.insertRow();
+		var namecell = newrow.insertCell();
+		namecell.innerHTML = names;
 		
-		"seedling":			loadImage("emoji/seedling.png"),
-		"checkered_flag": 	loadImage("emoji/checkered_flag.png"),
+		var imgcell = newrow.insertCell();
+		imgcell.innerHTML = "<img src='emoji/" + key + ".png'>"
 		
-		" ":   loadImage("emoji/space.png"),
-		"ERROR":  loadImage("emoji/NO_EMOJI.png"),
+		var usecell = newrow.insertCell();
+		if (glyph.use){
+			usecell.innerHTML = glyph.use
+		}
 	}
 }
 
@@ -101,6 +73,7 @@ function transcribeText(text){
 	// first we will just seperate the characters in brackets into a 1-dimensional list
 	for (var i = 0; i < text.length; i++){
 		var cchar = text.charAt(i);
+		var ccode = text.charCodeAt(i);
 		if (cchar == "["){
 			inbracket = true; continue;
 		}
@@ -125,8 +98,22 @@ function transcribeText(text){
 		}
 	}
 	
+	// all characters that are not associated with an emoji are converted to fullwidth		
+	for (var i = 0; i < textlist.length; i++){
+		var cword = textlist[i];
+		if (GLYPHS[cword]){ continue; }
+		
+		var shiftCharCode = Δ => c => String.fromCharCode(c.charCodeAt(0) + Δ);
+		var toFullWidth = str => str.replace(/[!-~]/g, shiftCharCode(0xFEE0));
+		var toHalfWidth = str => str.replace(/[！-～]/g, shiftCharCode(-0xFEE0));
+		
+		textlist[i] = toFullWidth(cword);
+	}
+	
 	// Its janky but this just add a space to the end so the last (or only) word is seen as a word
 	textlist.push(" ");
+	
+	print(textlist)
 	
 	// to word wrap, we will see if the current space character is on the same row as the last one
 	// if they are on different rows, then we know that the word has spilled into the next row.
@@ -192,6 +179,8 @@ function transcribeText(text){
 	resizeCanvas( canvaswidth, canvasheight );
 	clear();
 	
+	if (!TRANSPARENT){ background(54,57,63); }
+	
 	for (var i = 0; i < textlist.length; i++){
 		var x = ( i % TEXT_COLUMNS );
 		var y = Math.floor( i / TEXT_COLUMNS );
@@ -200,9 +189,15 @@ function transcribeText(text){
 }
 
 function drawGlyph(glyphname, column, row){
-	if (glyphname == " "){ return }
-	var img = GLYPHS[glyphname] ? GLYPHS[glyphname] : GLYPHS["ERROR"];
+	if (glyphname == " " || glyphname == "\n"){ return }
 	var x = CHAR_PADDING + ( column * ( CHAR_DIM + CHAR_PADDING ) );
-	var y = CHAR_PADDING + ( row * (CHAR_DIM + CHAR_PADDING ))
+	var y = CHAR_PADDING + ( row * (CHAR_DIM + CHAR_PADDING ))	
+	
+	if (glyphname.charCodeAt(0) >= 0xFF00 && glyphname.charCodeAt(0) <= 0xFFEF){
+		text(glyphname, x, y);
+		return;
+	}
+	
+	var img = GLYPHS[glyphname] ? GLYPHS[glyphname].img : GLYPHS["NO_EMOJI"].img;
 	image(img,x,y,CHAR_DIM,CHAR_DIM);
 }
