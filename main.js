f092c0fb841f85b3482c82756526558e5540bbd2
anakrusis <howtofamitracker@gmail.com>
2022-11-12 23:32:09 -0500

    updated website, added minecraft world history

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var maindiv = document.getElementById("maindiv"); 
 var innercode = maindiv.innerHTML;
 
-links = [ "https://soundcloud.com/amimifafa", "https://github.com/anakrusis", "https://twitter.com/amimifafa", "https://www.youtube.com/channel/UCcU35bFpdzi4v4SPdtHQp2Q", "https://battleofthebits.org/barracks/Profile/amelia/" ];
+links = [ "https://soundcloud.com/amimifafa", "https://github.com/anakrusis", "https://cohost.org/amimifafa", "https://www.youtube.com/channel/UCcU35bFpdzi4v4SPdtHQp2Q", "https://battleofthebits.org/barracks/Profile/amelia/" ];
 
-linktexts = [ "🎧 sound cloud", "🖥️ git hub", "🐤 twitter", "🎬youtube", "🏆 battle of bit" ];
+linktexts = [ "🎧 sound cloud", "🖥️ git hub", "🥚 cohost", "🎬youtube", "🏆 battle of bit" ];
 
 innercode+="<div class=\"titlebar\"><a href=\"\"><img class=\"headerimg\" src=\"./pog land.png\"></a></div>"
 innercode+="<table><tr>"
