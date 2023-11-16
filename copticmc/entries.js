3a9b743af6e744626da03cac46fa89f9e520d956
anakrusis <howtofamitracker@gmail.com>
2023-11-16 16:30:40 -0500

    added first table to translation notes

--- copticmc/entries.js
+++ copticmc/entries.js
@@ -3,7 +3,7 @@ ENTRIES = {
 		"coptic":	"ⲉⲛⲇⲉⲣⲙⲁⲛ",
 		"english":	"Enderman",
 		"etym":	["< proper name <i>Enderman</i>"],
-		"notes": "On the “Not to be translated” list of the Minecraft Official Glossary, with an exception: <i>\"In some languages this word is offensive, translate \'man\' and leave Ender unchanged.\"</i><br><br> Fully transliterating the name is the most common translation method. ⲉⲛⲇⲉⲣⲙⲁⲛ seems to be fine, but ⲣⲙⲛ̄ⲉⲛⲇⲉⲣ is a possibility as well.",
+		"notes": "On the “Not to be translated” list of the Minecraft Official Glossary, with an exception: <i>\"In some languages this word is offensive, translate \'man\' and leave Ender unchanged.\"</i><br><br> Fully transliterating the name is the most common method. ⲉⲛⲇⲉⲣⲙⲁⲛ seems to be fine, but a partial translation such as ⲣⲙⲛ̄ⲉⲛⲇⲉⲣ is a possibility as well.",
 		"tags": [ "etym-mod", "mob", "mob", "not-to-be-translated", "unattested", "vanilla" ]
 	},
 	"entity.Creeper.name": {
@@ -11,6 +11,20 @@ ENTRIES = {
 		"english":	"Creeper",
 		"etym":	["< proper name <i>Creeper</i>"],
 		"notes": "On the “Not to be translated” list of the Minecraft Official Glossary.<br><br>If this name existed in Coptic during its productive period, it would probably be spelled with all sorts of variations like ⲕⲣⲉⲓⲡⲉⲣ, ⲕⲣⲓⲡⲣ̄, etc.",
-		"tags": [ "etym-mod", "mob", "not-to-be-translated", "unattested" ]
+		"tags": [ "etym-mod", "mob", "not-to-be-translated", "unattested", "vanilla" ]
+	},
+	"generic.Birch.name": {
+		"coptic": "ⲗⲉⲩⲕⲏ",
+		"english": "White Poplar (\"Birch\")",
+		"etym": ["< Greek <i>λεύκη</i> \"white poplar\""],
+		"notes": "CDO C9617.<br><br>Plant names are among the most varied translations in the game, and substitutions are common. As I could not find a word for the birch tree attested in Coptic, I substituted a different kind of tree. The white poplar (<i>Populus alba</i>) has very similar white bark with black markings, and its wood is similarly \"white... with a slightly yellowish color\".<br>This word appears in Hosea 4:13 in the same sentence as ⲥⲉⲓ - Oak. It also appears in Isaiah 41:19.",
+		"tags": [ "etym-grk", "generic-material", "tree", "vanilla" ]
+	},
+	"tile.flower2.allium.name": {
+		"coptic": "ⲙ̄ϫⲱⲗ ϩⲟⲟⲩⲧ",
+		"english": "Allium",
+		"etym": ["", ""],
+		"notes": "Plant names are among the most varied translations in the game, and substitutions are common. Examples: <table><tr><td>Arabic:</td><td>خولنجان</td><td>\"galangal\" (<i>Alpinia officinarum</i> and similar plants in the ginger family <i>Zingiberaceae</i>, not in <i>Allium</i>)</td></tr><tr><td>Armenian:</td><td>սոխ</td><td>\"onion\"</td></tr><tr><td>Asturian:</td><td>Ayu bravu</td><td>\"wild garlic\"</td></tr><tr><td>Chinese:</td><td>绒球花</td><td>literally \"pom-pom flower\"</td></tr><tr><td>Hebrew:</td><td>שום</td><td>\"garlic\"</td></tr></table>",
+		"tags": [ "etym-sem", "etym-egy", "flower", "vanilla" ]
 	}
 }
