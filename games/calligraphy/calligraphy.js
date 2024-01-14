9a935c45244c9b5eb71b13436540c947deb88899
anakrusis <howtofamitracker@gmail.com>
2024-01-14 02:04:14 -0500

    canvas and scrolling viewport is working

--- games/calligraphy/calligraphy.js
+++ games/calligraphy/calligraphy.js
@@ -2,8 +2,14 @@
 // https://www.gorillasun.de/blog/simulating-brush-strokes-with-hookes-law-in-p5js-and-processing/
 
 function setup() {
-	maincanvas = createCanvas(1000, 1000);
-	background(255)
+	document.documentElement.style.overflow = 'hidden';  // firefox, chrome
+    document.body.scroll = "no"; // ie only
+	
+	// VIEWPORT CANVAS
+	vcanvas	= createCanvas(1000, 500);
+	// DOCUMENT CANVAS
+	dcanvas = createGraphics(500, 500);
+	dcanvas.background(255);
 	
 	//ellipseMode(CENTER);
 }
@@ -19,56 +25,98 @@ let vx = 0;
 let vy = 0;
 let splitNum = 100;
 let diff = 8;
+
+function mouseMoved() {
+	
+}
+
+function mousePressed() {
+	
+}
+
+function mouseWheel(e) {
+	
+}
+
+function keyPressed() {
+	
+}
+
 function draw() {
-
+	if (keyIsDown(87)) { // up
+		cam_y--;
+	}
+	else if (keyIsDown(83)) { // down
+		cam_y++;
+	}
 	
-  if(mouseIsPressed) {
-    if(!f) {
-      f = true;
-      x = mouseX;
-      y = mouseY;
-    }
+	if (keyIsDown(65)) { // left
+		cam_x--;
+	}
+	if (keyIsDown(68)) { // right
+		cam_x++;
+	}
 	
-    vx += ( mouseX - x ) * spring;
-    vy += ( mouseY - y ) * spring;
-    vx *= friction;
-    vy *= friction;
+	clear();
+	image(dcanvas, tra_x(0), tra_y(0));
+	
+	if(mouseIsPressed) {
+		// MIDDLE CLICK: scroll canvas
+		if (mouseButton === CENTER) {
+			cam_x -= (movedX / cam_zoom);
+			cam_y -= (movedY / cam_zoom);
+			
+		// LEFT CLICK: draw brush strokes to the canvas
+		}else if (mouseButton === LEFT){
+			if(!f) {
+				f = true;
+				x = mouseX;
+				y = mouseY;
+			}
 
-    v += sqrt( vx*vx + vy*vy ) - v;
-    v *= 0.6;
+			vx += ( mouseX - x ) * spring;
+			vy += ( mouseY - y ) * spring;
+			vx *= friction;
+			vy *= friction;
 
-    oldR = r;
-    r = brushSize - v;
+			v += sqrt( vx*vx + vy*vy ) - v;
+			v *= 0.6;
 
-    for( let i = 0; i < splitNum; ++i ) {
-      oldX = x;
-      oldY = y;
-      x += vx / splitNum;
-      y += vy / splitNum;
-      oldR += ( r - oldR ) / splitNum;
-      if(oldR < 1) { oldR = 1; }
-	  
-	  //push()
-	  //translate(x,y)
-	  //rotate(PI/2)
-	  
-	  //rx = (x * cos( -PI/2 )) - (y * sin( -PI / 2))
-	  //ry = (x * sin( -PI/2 )) + (y * cos( -PI/2))
-	  
-	  fill(0);
-	  ellipse( x, y, oldR, oldR / 2);
-	  
-	  //pop();
-	  
-      //strokeWeight( oldR+diff );  // AMEND: oldR -> oldR+diff
-      //line( x, y, oldX, oldY );
-      //strokeWeight( oldR );  // ADD
-      //line( x+diff*1.5, y+diff*2, oldX+diff*2, oldY+diff*2 );  // ADD
-      //line( x-diff, y-diff, oldX-diff, oldY-diff );  // ADD
-    }
+			oldR = r;
+			r = brushSize - v;
 
-  } else if(f) {
-    vx = vy = 0;
-    f = false;
-  }
+			for( let i = 0; i < splitNum; ++i ) {
+				oldX = x;
+				oldY = y;
+				x += vx / splitNum;
+				y += vy / splitNum;
+				oldR += ( r - oldR ) / splitNum;
+				if(oldR < 1) { oldR = 1; }
+
+				//push()
+				//translate(x,y)
+				//rotate(PI/2)
+
+				//rx = (x * cos( -PI/2 )) - (y * sin( -PI / 2))
+				//ry = (x * sin( -PI/2 )) + (y * cos( -PI/2))
+
+				dcanvas.fill(0);
+				var ux = untra_x(x,y); var uy = untra_y(x,y);
+				console.log("ux: " + ux + " uy: " + uy );
+				dcanvas.ellipse( ux, uy, oldR, oldR / 2);
+
+				//pop();
+
+				//strokeWeight( oldR+diff );  // AMEND: oldR -> oldR+diff
+				//line( x, y, oldX, oldY );
+				//strokeWeight( oldR );  // ADD
+				//line( x+diff*1.5, y+diff*2, oldX+diff*2, oldY+diff*2 );  // ADD
+				//line( x-diff, y-diff, oldX-diff, oldY-diff );  // ADD
+			}
+
+		} 
+	} else if(f) {
+		vx = vy = 0;
+		f = false;
+	}
 }
