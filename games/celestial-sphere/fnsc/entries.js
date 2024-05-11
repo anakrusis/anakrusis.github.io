a2f5b9abc687e2585dd3f042bcf2e1d160697eb8
anakrusis <howtofamitracker@gmail.com>
2024-05-11 18:17:45 -0400

    added more entries to the funny star catalogue

--- games/celestial-sphere/fnsc/entries.js
+++ games/celestial-sphere/fnsc/entries.js
@@ -1,6 +1,30 @@
 ENTRIES = [
+	{
+		"title": "HD 69",
+		"text": "<h3><span class=\"ns\">❌ not yet seen</span></h3>Right ascension: 00h 05m 22s<br>Declination: -79° 15′ 14″<br>Apparent magnitude: 7.7<br><br>A faint star in Octans that never rises at all from my location, so there is absolutely no chance of seeing it."
+	},
+	{
+		"title": "HD 420",
+		"text": "<h3><span class=\"ns\">❌ not yet seen</span></h3>Right ascension: 00h 08m 48s<br>Declination: 05° 05′ 50″<br>Apparent magnitude: 8.0<br><br>This faint star in Pisces is probably barely visible in my binoculars when Pisces is high in the sky. That will have to wait till next year, because all these low HD numbers are near the spring equinox point, and (as of 11 May 2024) that point is long since sunk in the western sky."
+	},
 	{ 
 		"title": "69 Orionis and HD 42069",
-		"text": "<div class=\"left\"><img src=\"img/69 ori.png\"></div>These two stars are only about one degree apart in the sky, located in Orion's club.<h3><span class=\"ns\">69 Orionis — ❌ not yet seen</span></h3>Right ascension: 06h 12m 03s<br>Declination: 16° 07′ 50″<br>Apparent magnitude: 4.9<br><br>This is a faint star to see with the naked eye; I could never see it under my light polluted skies. It would be easily seen in binoculars, but I've never looked for it yet. At this time of year (11 May 2024) Orion sets early in the evening, so I should try to spot it on the soonest clear night.<br><br>~~~<h3><span class=\"ns\">HD 42069 — ❌ not yet seen</span></h3>Right ascension: 06h 09m 25s<br>Declination: 17° 10′ 60″<br>Apparent magnitude: 8.8<br><br>Would be too faint to see even in my low powered binoculars. I've seen some 8th magnitude stars before, but never near the western horizon and never as faint as 8.8."
+		"text": "<div class=\"left\"><img src=\"img/69 ori.png\"></div>These two auspiciously-numbered stars happen to be just one degree apart in the sky, located in Orion's club.<br><h3><span class=\"ns\">69 Orionis — ❌ not yet seen</span></h3>Right ascension: 06h 12m 03s<br>Declination: 16° 07′ 50″<br>Apparent Apparent magnitude: 4.9<br><br>This would be a bright star to see in binoculars, but I've never looked for it yet. Some can even see it with the naked eye, but I can't see any fourth magnitude stars due to light pollution. At this time of year (11 May 2024) Orion sets early in the evening, so I should try to spot it with binoculars on the soonest clear night.<br><br>~~~<h3><span class=\"ns\">HD 42069 — ❌ not yet seen</span></h3>Right ascension: 06h 09m 25s<br>Declination: 17° 10′ 60″<br>Apparent Apparent magnitude: 8.8<br><br>Would be too faint to see even in my low powered binoculars. I've seen some 8th magnitude stars before, but never near the western horizon and never as faint as 8.8."
+	},
+	{
+		"title": "HD 69420",
+		"text": "<div class=\"left\"><img src=\"img/hd 69420.png\"></div><h3><span class=\"ns\">❌ not yet seen</span></h3>Right ascension: 08h 16m 04s<br>Declination: -23° 52′ 18″<br>Apparent magnitude: 10.1<br><br>This star in Puppis is very faint for the HD Catalogue, and hardly stands out among the other faint background stars. It is located about two degrees east of the bright star Rho Puppis (Tureis). There is no chance of seeing it with my binoculars.<br><br>There are 1000 stars with HD numbers 69xxx, all around right ascension 08h 14~22m, which runs along a line through Ursa Minor, Camelopardalis, Ursa Major, Lynx, Cancer, Hydra, Puppis, Vela, Carina, Volans, Chamaeleon and Octans. Most of these are not particularly funny numbers, but I made an exception for 69420."
+	},
+	{
+		"title": "HIP 42069 (HD 73127)",
+		"text": "<h3><span class=\"ns\">❌ not yet seen</span></h3>Right ascension: 08h 34m 29s<br>Declination: -51° 05′ 35″<br>Apparent magnitude: 6.6<br><br>This star in Vela is bright enough to be seen pretty easily in binoculars in theory. And from my location it culminates about 12° above the horizon, which is plenty high enough to be seen, but it's probably too faint to see in the light polluted southern sky. And I have just missed the season to see it anyways; it is near Delta Velorum in right ascension (08h 44m 42s), which is just about sunk by now (11 May 2024). I might be able to see it around culmination next year."
+	},
+	{
+		"title": "69 Ursae Majoris (δ UMa, Megrez)",
+		"text": "<div class=\"left\"><img src=\"img/megrez.png\"></div><h3><span class=\"seen\">✅ seen many times, sketched 2024 March 3</span></h3>Right ascension: 12h 15m 26s<br>Declination: 57° 01′ 57″<br>Apparent magnitude: 3.3<br><br>As part of the Big Dipper, and an easy naked eye star to spot, I cannot tell when I first saw it, as I've lived in the Northern Hemisphere my whole life.<br>I first sketched it on a cloudy night as it was rising above the roof of the house. It appears very bright in binoculars.<br><br>There are not many constellations with Flamsteed designations up to 69, so that is why I have included it, even though Megrez's Flamsteed number is rarely used."
+	},
+	{
+		"title": "HIP 69420 (HD 123873)",
+		"text": "<h3><span class=\"ns\">❌ not yet seen</span></h3>Right ascension: 14h 12m 50s<br>Declination: -66° 27′ 09″<br>Apparent magnitude: 8.1<br><br>A faint star in Circinus that never rises at all from my location, so there is absolutely no chance of seeing it."
 	}
 ]
