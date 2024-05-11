586023a89d11e58d7a9020d5d843e68b95939155
anakrusis <howtofamitracker@gmail.com>
2024-05-11 14:59:52 -0400

    started funny number stars catalogue

---
+++ games/celestial-sphere/fnsc/entries.js
@@ -0,0 +1,6 @@
+ENTRIES = [
+	{ 
+		"title": "69 Orionis and HD 42069",
+		"text": "<div class=\"left\"><img src=\"img/69 ori.png\"></div>These two stars are only about one degree apart in the sky, located in Orion's club.<h3><span class=\"ns\">69 Orionis — ❌ not yet seen</span></h3>Right ascension: 06h 12m 03s<br>Declination: 16° 07′ 50″<br>Apparent magnitude: 4.9<br><br>This is a faint star to see with the naked eye; I could never see it under my light polluted skies. It would be easily seen in binoculars, but I've never looked for it yet. At this time of year (11 May 2024) Orion sets early in the evening, so I should try to spot it on the soonest clear night.<br><br>~~~<h3><span class=\"ns\">HD 42069 — ❌ not yet seen</span></h3>Right ascension: 06h 09m 25s<br>Declination: 17° 10′ 60″<br>Apparent magnitude: 8.8<br><br>Would be too faint to see even in my low powered binoculars. I've seen some 8th magnitude stars before, but never near the western horizon and never as faint as 8.8."
+	}
+]
